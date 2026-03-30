0dda065257962636b719b2e719014a1c9d3dfd08
RoscaStefanAndrei <rosca.d.stefanandrei23@stud.rau..ro>
2026-03-30 09:54:48 +0300

    fix: Update superuser password on each build deploy

--- build.sh
+++ build.sh
@@ -11,4 +11,20 @@ python manage.py migrate
 # Create superuser from env vars (skips if already exists)
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
     python manage.py createsuperuser --no-input || true
+    # Update password in case it changed
+    if [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+        python -c "
+import django; django.setup()
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+try:
+    u = User.objects.get(username=os.environ['DJANGO_SUPERUSER_USERNAME'])
+    u.set_password(os.environ['DJANGO_SUPERUSER_PASSWORD'])
+    u.save()
+    print(f'Password updated for {u.username}')
+except User.DoesNotExist:
+    print('Superuser not found')
+"
+    fi
 fi
