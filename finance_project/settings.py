b5b573c976881bfb07732506dee3d702c0a61c25
RoscaStefanAndrei <rosca.d.stefanandrei23@stud.rau..ro>
2026-03-30 09:49:25 +0300

    fix: Add default file storage to STORAGES config
    STORAGES was missing the 'default' key, causing InvalidStorageError
    when templates rendered file field URLs (e.g. profile avatars).

--- finance_project/settings.py
+++ finance_project/settings.py
@@ -162,6 +162,9 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
@@ -172,9 +175,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Login/Logout redirect
-LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
-LOGIN_URL = 'login'
 
 # Default primary key type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
