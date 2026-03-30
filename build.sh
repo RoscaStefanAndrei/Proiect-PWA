8da5dcdc12bcd981b6fe42140f523edda1522d38
RoscaStefanAndrei <rosca.d.stefanandrei23@stud.rau..ro>
2026-03-30 10:24:49 +0300

    fix: Set DJANGO_SETTINGS_MODULE in build script for inline Python

--- build.sh
+++ build.sh
@@ -8,15 +8,16 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+export DJANGO_SETTINGS_MODULE=finance_project.settings
+
 # Create superuser from env vars (skips if already exists)
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
     python manage.py createsuperuser --no-input || true
     # Update password in case it changed
     if [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
         python -c "
-import django; django.setup()
+import django, os; django.setup()
 from django.contrib.auth import get_user_model
-import os
 User = get_user_model()
 try:
     u = User.objects.get(username=os.environ['DJANGO_SUPERUSER_USERNAME'])
